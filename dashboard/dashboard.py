939cb1bd39286540ff86dd5091b2f6832a9b9bca
vladimir-chernykh <vladimir.chernykh@phystech.edu>
2020-01-14 20:55:16 +0300

    comments

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,7 +2,6 @@ import os
 import re
 import glob
 
-import numpy as np
 import pandas as pd
 
 from multiprocessing import Manager
@@ -13,41 +12,48 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 
+# path to the folder where metric files are stored
 SCORES_DIR = os.getenv("DASHBOARD_SCORES_DIR", "./results")
 
-external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
-
+# dataframe which will store scores in RAM
 df_scores = pd.DataFrame([["boston_house_prices_train", 6.303904029016756],
                           ["boston_house_prices_val", 7.554702970297035]],
                          columns=["file", "baseline"])
 
+# list of dataframes with metrics
 df_list = []
+# list of model names
 full_model_list = []
 
+# lock manager to update the table and the graph properly when new files arrives
 manager = Manager()
 lock = manager.Lock()
 
 
 def update_table(lock):
+    """ Updates the data for table of results when the new metrics file arrives. """
 
     lock.acquire()
 
+    # get list of all metrics files
     df_path_list = glob.glob(os.path.join(SCORES_DIR, "*.csv"))
     df_path_list.sort(key=os.path.getmtime)
 
     for df_path in df_path_list:
-        if "scores.csv" not in df_path:
-            model_id = df_path.split("/")[-1].strip(".csv")
-            if model_id not in full_model_list:
-                df_temp = pd.read_csv(df_path)[["mae"]]
-                df_temp.columns = ["model_" + model_id]
-                full_model_list.append(model_id)
-                df_list.append(df_temp)
+        model_id = df_path.split("/")[-1].strip(".csv")
+        if model_id not in full_model_list:
+            # add to lists if there is new file
+            df_temp = pd.read_csv(df_path)[["mae"]]
+            df_temp.columns = ["model_" + model_id]
+            full_model_list.append(model_id)
+            df_list.append(df_temp)
 
     lock.release()
 
 
 def cell_style(value, threshold_value):
+    """ Paints row to green or red depending on the threshold value. """
+
     if value <= threshold_value:
         style = {
             "backgroundColor": "#e6ffef",
@@ -60,6 +66,7 @@ def cell_style(value, threshold_value):
 
 
 def generate_table():
+    """ Generates new HTML page with dashboard table of results when the new metrics file arrives. """
 
     update_table(lock)
 
@@ -88,6 +95,8 @@ def generate_table():
 
 
 def serve_dashboard_layout():
+    """ Serves HTML page with table of results. """
+
     return html.Div([
         html.H4(children="Metric by file"),
         generate_table()
@@ -95,6 +104,8 @@ def serve_dashboard_layout():
 
 
 def serve_graph_layout(default="boston_house_prices_val"):
+    """ Serves HTML pages with graph of metric evolution. """
+
     update_table(lock)
     xaxisrange = list(range(1, len(df_list) + 1))
     df = pd.concat([df_scores[["file"]]] + df_list, axis=1)
@@ -145,16 +156,19 @@ def serve_graph_layout(default="boston_house_prices_val"):
     ])
 
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+# create Dash web-application
+app = dash.Dash(__name__, external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"])
 
 app.config.suppress_callback_exceptions = True
 
+# create app layout
 app.layout = html.Div([
     dcc.Location(id="url", refresh=False),
     html.Div(id="page-content")
 ])
 
 
+# create dropdown menu for switching between graphs
 @app.callback(
     Output(component_id="quality-graph", component_property="figure"),
     [Input(component_id="dist-drop", component_property="value")]
@@ -191,6 +205,7 @@ def update_graph(value):
             }
 
 
+# callback to distinguish between different HTML "pages" with content
 @app.callback(dash.dependencies.Output("page-content", "children"),
               [dash.dependencies.Input("url", "pathname")])
 def display_page(pathname):
@@ -204,5 +219,6 @@ def display_page(pathname):
         return serve_dashboard_layout()
 
 
+# run app
 if __name__ == "__main__":
     app.run_server(debug=False, port=7050, host="0.0.0.0")
